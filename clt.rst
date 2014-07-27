6e737d1398da322f01abda17e7f0cc4c3240237f
Juergen Meinecke <juergen.meinecke@gmail.com>
2014-07-27 21:25:44 +1000

    fixed some things

---
+++ clt.rst
@@ -0,0 +1,7 @@
+Illustration of Central Limit Theorem
+========================================
+
+Bla bla blu
+
+.. image:: clt.png
+
--- conf.py
+++ conf.py
@@ -24,7 +24,8 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'matplotlib.sphinxext.plot_directive']
+#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'matplotlib.sphinxext.plot_directive']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
